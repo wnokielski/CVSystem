888790a21cf1d5cc5eca12a394d4695a93187ce1
Wojciech Nokielski <61920689+wnokielski@users.noreply.github.com>
2023-04-01 20:10:24 +0200

    [WN] DB Configuration (#2)
    * [WN] Project structure, SQLAlchemy configuration, changes in docker-compose.yml, added requirements.txt file
    
    * Update requirements

--- CVSystemAPI/src/main.py
+++ CVSystemAPI/src/main.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
-from src.auth.router import router as auth_router
+from src.v1.auth.router import router as auth_router
+from src.core.models.base import Base
+from src.core.utils.database import DB
+
+engine = DB.get_instance().engine
+Base.metadata.create_all(engine)
 
 app = FastAPI()
 
